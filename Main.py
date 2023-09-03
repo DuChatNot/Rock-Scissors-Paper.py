1c4eb7a6089baa4ec0a1adc6bb58e6c20030ec3d
duChat <cpantoja022@gmail.com>
2023-09-03 15:40:28 -0600

    Mayor changes, base game finished

--- Main.py
+++ Main.py
@@ -4,59 +4,70 @@ import random #importación de modulo de aleatoriedad
 round_selection = int(input('Cuantos Rounds? '))
 rounds = 0
 
+user_w = 0
+comp_w = 0
+
 while rounds < round_selection:
+
+    if user_w > round_selection/2 or comp_w > round_selection/2: # Prediction
+        rounds == round_selection
+        break
+
     selection = ('piedra','papel','tijera')
 
-    user = input('Piedra, Papel o Tijera? => ')
+    print('....................................................................................')
+    print(f'SCORE: User: {user_w} --- --- CPU: {comp_w}  || Round: {rounds}/{round_selection}')
+    user = input('Piedra - Papel - Tijera ===> ')
     comp = random.choice(selection)
 
     user = user.lower()
     comp = comp.lower()
 
-    user_w = 0
-    comp_w = 0
-
     if user == 'piedra' or user == 'papel' or user == 'tijera':
-        print('-- -- -- -- --')
-        print('User Selection => ', user)
-        print('Computer Selection => ', comp)
+        print('...........Results...........')
+        print('-- User => ', user.upper())
+        print('-- CPU => ', comp.upper())
         rounds += 1
-        print('-- -- -- -- --')
 
-    print(f"Round {rounds}/{round_selection}")
     if user == comp:
-        print('========================> Empate')
+        print('==========================================================> Empate')
     elif user == 'piedra':
         if comp == 'tijera':
-            print('========================> User gana')
+            print('==========================================================> User gana')
             user_w += 1
         else:
-            print('========================> CPU gana')
+            print('===========================================================> CPU gana')
             comp_w +=1
 
     elif user == 'papel':
         if comp == 'piedra':
-            print('========================> User gana')
+            print('==========================================================> User gana')
             user_w += 1
         else:
-            print('========================> CPU gana')
+            print('==========================================================> CPU gana')
             comp_w +=1
 
     elif user == 'tijera':
         if comp == 'papel':
-            print('========================> User gana')
+            print('==========================================================> User gana')
             user_w += 1
         else:
-            print('========================> CPU gana')
+            print('==========================================================> CPU gana')
             comp_w +=1
     else:
         print('Invalid User Input')
         continue
 
+print('----------------------------------------------------------------------------------------')
+print(' ')
+
 if rounds == round_selection:
     if user_w > comp_w:
-        print('Victoria de User')
-    elif user_w < comp_w:
-        print('Victoria CPU')
+        print('<=======================> Victoria de User <=======================> ')
+    elif comp_w > user_w:
+        print('<=======================> Victoria CPU <=======================> ')
     else:
-        print('EMPATE')
+        print('<=================================================> EMPATE <=================================================> ')
+
+print(' ')
+print('----------------------------------------------------------------------------------------')
